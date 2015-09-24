68aad4f93cca249ee879de7db54ce3707fa61911
ZhengXu <zheng.xu@uta.edu>
2015-09-24 17:45:03 -0500

    Try add setup.py to install with pip

---
+++ setup.py
@@ -0,0 +1,25 @@
+__author__ = 'zhengxu'
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "ProtoText",
+    version = "0.1",
+    packages = find_packages(exclude=['tests']),
+
+    # Project uses reStructuredText, so ensure that the docutils get
+    # installed or upgraded on the target machine
+    install_requires = ['protobuf>=2.6'],
+
+    package_data = {},
+
+    # metadata for upload to PyPI
+    author = "Zheng Xu",
+    author_email = "xuzheng1111@gmail.com",
+    description = "ProtoText is a powerful python dict-like wrapper class to process google protobuf objects.",
+    license = "MIT",
+    keywords = "protobuf, dict",
+    url = "https://github.com/XericZephyr/prototext",   # project home page, if any
+
+    # could also include long_description, download_url, classifiers, etc.
+)
