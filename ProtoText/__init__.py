becd81d60b5864dc2cd44b107995722a97762433
ZhengXu <xuzheng1111@gmail.com>
2015-11-13 00:09:46 -0600

    Push version to 0.3.5 and upload to PyPI

--- ProtoText/__init__.py
+++ ProtoText/__init__.py
@@ -2,7 +2,7 @@ from .models import MessageWrapper
 from .hook_helper import register_class_hook, deregister_class_hook
 
 __author__ = 'zhengxu'
-__version__ = '0.3.4'
+__version__ = '0.3.5'
 
 try:
     from google.protobuf.message import Message
